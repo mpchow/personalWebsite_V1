60bc5285a0963ebb8de3d620368fe25b5e3c35e5
mpchow <mattchow918@gmail.com>
2020-01-06 20:14:03 -0800

    Added scroll animations and link to resume

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
-function scrollToElement() {
-    
+function scrollToAbout() {
+    let element = document.getElementById("intro");
+    element.scrollIntoView({behavior: "smooth"});
+    return false;
+}
+
+function scrollToProjects() {
+    let element = document.getElementById("projectsTitle");
+    element.scrollIntoView({behavior: "smooth"});
+    return false;
+}
+
+function scrollToContact() {
+    let element = document.getElementById("contact");
+    element.scrollIntoView({behavior: "smooth"});
+    return false;
 }
