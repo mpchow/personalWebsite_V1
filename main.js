164bdaa49b523a0f7ec76dbf204f8c2d28208998
mpchow <mattchow918@gmail.com>
2020-02-18 00:13:34 -0800

    Changed resume format from google drive link to additional page

--- main.js
+++ main.js
@@ -16,28 +16,3 @@ function scrollToContact() {
     return false;
 }
 
-'use strict';
-
-const e = React.createElement;
-
-class LikeButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { liked: false };
-  }
-
-  render() {
-    if (this.state.liked) {
-      return 'You liked this.';
-    }
-
-    return e(
-      'button',
-      { onClick: () => this.setState({ liked: true }) },
-      'Like'
-    );
-  }
-}
-
-const domContainer = document.querySelector('#like_button_container');
-ReactDOM.render(e(LikeButton), domContainer);
