6bae1cca86dc88a5cd1220fd3e71942c91ba9668
mpchow <mattchow918@gmail.com>
2020-02-25 17:12:17 -0800

    Added animation for name

--- main.js
+++ main.js
@@ -1,15 +1,43 @@
 window.addEventListener('load', ()=> {
     let mainPage = document.getElementById("mainPage");
     mainPage.style.opacity = 1;
+    let mainTitle = document.getElementById('name').style;
+    mainTitle.opacity = 0;
     window.setTimeout( disableLoader, 2000); 
+    window.setTimeout(fadeInTitle, 2500);
+    window.setTimeout(fadeLinksIn, 2600);
 });
+
+function fadeLinksIn() {
+
+    
+}
+
+function fadeInTitle () {
+    let mainTitle = document.getElementById('name').style;
+    mainTitle.opacity = 0;
+    let opacity = 0;
+    fadeNameIn();
+
+    function fadeNameIn() {
+        mainTitle.opacity = opacity;
+        opacity += 0.1;
+        if(mainTitle.opacity > 1) {
+            mainTitle.display = "flex";
+        }
+        else {
+            setTimeout(fadeNameIn, 40);
+        }
+    }
+}
+
+
 
 function disableLoader() {
     let loader = document.getElementById('loader').style;
     loader.opacity = 1;
 
      (function fadeOut(){(loader.opacity-=.1)<0?loader.display="none":setTimeout(fadeOut,40)})();
-
 }
 
 function scrollToAbout() {
